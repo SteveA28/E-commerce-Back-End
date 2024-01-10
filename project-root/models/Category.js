c4a3ed73ba3b0ab13e41fb9fb4c9fdb2f83499e5
SteveA28 <alonsosteven32@gmail.com>
2024-01-10 17:17:28 -0500

    added models

---
+++ project-root/models/Category.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const Product = require('./Product');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    modelName: 'category',
+  }
+);
+
+// Add associations
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+module.exports = Category;
