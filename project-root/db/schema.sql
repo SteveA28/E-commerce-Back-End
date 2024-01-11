4e6e9415eaf2f19afec5ac7194713faaed5ee55e
SteveA28 <alonsosteven32@gmail.com>
2024-01-11 00:31:04 -0500

    added schmea

---
+++ project-root/db/schema.sql
@@ -0,0 +1,30 @@
+-- Create the Category table
+CREATE TABLE category (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(255) NOT NULL
+);
+
+-- Create the Product table
+CREATE TABLE product (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(255) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  stock INT NOT NULL DEFAULT 10,
+  category_id INT,
+  FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+-- Create the Tag table
+CREATE TABLE tag (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(255) NOT NULL
+);
+
+-- Create the ProductTag table
+CREATE TABLE productTag (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_id INT,
+  tag_id INT,
+  FOREIGN KEY (product_id) REFERENCES product(id),
+  FOREIGN KEY (tag_id) REFERENCES tag(id)
+);
